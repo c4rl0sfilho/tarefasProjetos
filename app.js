54970cd169b3814b052b7f57d2f8a91acd7ae45c
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-02-06 16:19:07 -0300

    back feito

--- app.js
+++ app.js
@@ -14,3 +14,33 @@ function CriarTarefa(){
     //containerTarefa.appendChild(tituloTarefa)
 
 }
+
+async function validarLogin(){
+    const email = document.getElementById('email').value; 
+    const senha = document.getElementById('senha').value; 
+
+
+    if(email === '' || senha === '' ){
+        alert('Preencha os campos corretamente !!!')
+    } 
+
+    try{
+        const responseApi = await fetch('http://localhost:5080/usuario')
+        const listUsers = await responseApi.json();
+
+        listUsers.forEach((user) => {
+
+            console.log(user.email)
+            console.log(email)
+            if(email === user.email && senha === user.senha){
+                alert('Usuário Logado com Sucesso !!!');
+                window.location.href = '../tarefas.html';
+            }
+        })
+
+    } catch (error){
+        console.error(error);
+    }
+
+}
+
